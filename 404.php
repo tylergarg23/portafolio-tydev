d8710286dc3bbfb04f41db08753696d5acad5833
tyler26_ <tylergarg@gmail.com>
2025-04-20 00:22:39 -0500

    add page 404 not found

---
+++ 404.php
@@ -0,0 +1,37 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon-logo.ico">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+  <?php wp_head(); ?>
+</head>
+
+<div class="bg-purple">
+  <div class="stars">
+    <div class="custom-navbar">
+      <div class="central-body">
+        <img class="image-404" src="/wp-content/themes/tylerdev/img/404.png" width="300px" alt="404-png">
+        <h1 class="title-not-foud">¡Ups! Página no encontrada 😢</h1>
+        <p class="description-not-found">No pudimos encontrar lo que estabas buscando.</p>
+        <a href="/" class="btn-go-home" target="">Volver al inicio</a>
+      </div>
+      <div class="objects">
+        <img class="object_rocket" src="/wp-content/themes/tylerdev/img/rocket.svg" width="40px">
+        <div class="earth-moon">
+          <img class="object_earth" src="/wp-content/themes/tylerdev/img/earth.svg" width="100px">
+          <img class="object_moon" src="/wp-content/themes/tylerdev/img/moon_1.svg" width="80px">
+        </div>
+        <div class="box_astronaut">
+          <img class="object_astronaut" src="/wp-content/themes/tylerdev/img/astronaut_1.svg" width="140px" alt="astronaut-svg">
+        </div>
+      </div>
+      <div class="glowing_stars">
+        <div class="star"></div>
+        <div class="star"></div>
+        <div class="star"></div>
+        <div class="star"></div>
+        <div class="star"></div>
+      </div>
+    </div>
+  </div>
